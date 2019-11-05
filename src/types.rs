6127b7fc54112907ab65a75c09d45150d04c8e25
Greg Kapka <greg@oraclize.it>
2019-11-05 15:21:39 +0000

    feat(types): adds EosBlockJson to types

--- src/types.rs
+++ src/types.rs
@@ -13,18 +13,41 @@ pub type EosActionReceipts = Vec<EosActionReceipt>;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct EosInputJson {
-    pub block: String, // TODO When we know the type better!
+    pub block: EosBlockJson,
     pub actions: Vec<EosActionJson>,
     pub action_receipts: Vec<EosActionReceiptJson>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct EosBlockJson {
+    pub block_id: String,
+    pub block_num: u64,
+    pub producer: String,
+    pub confirmed: usize,
+    pub previous: String,
+    pub timestamp: String,
+    pub action_mroot: String,
+    pub schedule_version: usize,
+    pub transaction_mroot: String,
+    pub producer_signature: String,
+    pub new_producers: serde_json::Value,
+    pub transactions: Vec<serde_json::Value>, // TODO Real type for this!
+    pub block_extensions: Vec<serde_json::Value>,
+    pub header_extensions: Vec<serde_json::Value>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct EosActionJson {
-    pub data: String,
     pub name: String,
     pub account: String,
-    pub raw_data: String,
-    pub authorization: Vec<(String, u64)>,
+    pub data: serde_json::Value, // NOTE: Could be hex string, or contract data!
+    pub authorization: Vec<AuthorizationJson>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AuthorizationJson {
+    pub actor: String,
+    pub permission: String,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -32,8 +55,8 @@ pub struct EosActionReceiptJson {
     pub receiver: String,
     pub act_digest: String,
     pub global_sequence: u64,
-    pub recv_sequence: u64,
-    pub auth_sequence: (String, u64), // FIXME: Can serde_json deserialize a tuple?
+    pub recv_sequence:  u64,
+    pub auth_sequence: Vec<(String, u64)>,
     pub code_sequence: usize,
     pub abi_sequence: usize,
 }
