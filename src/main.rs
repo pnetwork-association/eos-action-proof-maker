3b6d1898a049a7815159cc0a5ff65525f11bfeae
Greg Kapka <greg@oraclize.it>
2019-11-05 13:35:59 +0000

    feat(main): adds in completed pipeline so far

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+pub mod state;
+pub mod types;
+pub mod error;
+pub mod constants;
+pub mod usage_info;
+pub mod parse_cli_args;
+pub mod parse_input_json;
+pub mod initialize_logger;
+
+#[macro_use] extern crate log;
+#[macro_use] extern crate serde_derive;
+
+use crate::{
+    initialize_logger::initialize_logger,
+    parse_cli_args::parse_cli_args_and_put_in_state,
+    parse_input_json::parse_input_json_string_and_put_in_state,
+};
+
 fn main() {
-    println!("Hello, world!");
+    match parse_cli_args_and_put_in_state()
+        .and_then(parse_input_json_string_and_put_in_state)
+        {
+            Ok(state) => {
+                println!("{:?}", state)
+            },
+            Err(e) => {
+                error!("{}", e);
+                println!("{}", e);
+                std::process::exit(1);
+            }
+        }
 }
