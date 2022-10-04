bf9ba65ab9b220a5fd9e97527bee9004da1571ad
Greg Kapka <greg@oraclize.it>
2022-10-04 16:32:59 +0100

    feat(outputter): <- update that to use correct digest type

--- src/generate_output.rs
+++ src/generate_output.rs
@@ -1,4 +1,5 @@
 use crate::{
+    action_return_values::action_return_value_protocol_feature_is_enabled,
     get_action_digest::get_action_digest,
     state::State,
     types::{Output, Result},
@@ -7,6 +8,12 @@ use eos_chain::{Digest, SerializeData};
 use serde_json;
 
 pub fn generate_output_string(state: State) -> Result<String> {
+    let action = state.get_eos_action()?;
+    let action_receipts = state.get_eos_action_receipts()?;
+
+    let action_return_value_is_enabled =
+        action_return_value_protocol_feature_is_enabled(action, action_receipts)?;
+
     Ok(serde_json::to_string(&Output {
         tx_id: state.get_eos_input_json()?.action_receipts[state.get_proof_index()? as usize]
             .tx_id
@@ -16,20 +23,19 @@ pub fn generate_output_string(state: State) -> Result<String> {
         action_proof: state.get_merkle_proof()?.to_vec(),
         action_digest: format!(
             "0x{}",
-            hex::encode(get_action_digest(state.get_eos_action()?)?)
+            hex::encode(get_action_digest(action, action_return_value_is_enabled)?)
         ),
-        serialized_action: hex::encode(state.get_eos_action()?.to_serialize_data()?),
+        serialized_action: hex::encode(action.to_serialize_data()?),
         action_json: state.get_eos_input_json()?.action.clone(),
         action_receipt_json: state.get_eos_input_json()?.action_receipts
             [state.get_proof_index()? as usize]
             .clone(),
         action_receipt_digest: format!(
             "0x{}",
-            state.get_eos_action_receipts()?[state.get_proof_index()? as usize].digest()?,
+            action_receipts[state.get_proof_index()? as usize].digest()?,
         ),
         serialized_action_receipt: hex::encode(
-            state.get_eos_action_receipts()?[state.get_proof_index()? as usize]
-                .to_serialize_data()?,
+            action_receipts[state.get_proof_index()? as usize].to_serialize_data()?,
         ),
     })?)
 }
